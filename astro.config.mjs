8a7d899e73bde1b4ced411463fa4d89e08896434
conor-ob <conor.w.ob@gmail.com>
2024-11-24 17:15:54 +0000

    install tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwind from '@astrojs/tailwind';
+
 // https://astro.build/config
 export default defineConfig({
-  output: 'static'
+  site: import.meta.env.VITE_SITE,
+  output: 'static',
+  integrations: [tailwind()]
 });
