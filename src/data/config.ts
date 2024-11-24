cf1d43bb5bb8562662c3a010b6dc1cd9b4863167
conor-ob <conor.w.ob@gmail.com>
2024-11-24 20:41:48 +0000

    add some jobs

---
+++ src/data/config.ts
@@ -0,0 +1,21 @@
+export const config = {
+  meta: {
+    title: "Conor O'Brien",
+    description: "Conor O'Brien's CV",
+  },
+  header: {
+    title: "cv",
+  },
+  footer: {
+    links: [
+      {
+        name: "LinkedIn",
+        url: "https://www.linkedin.com/in/conor-ob",
+      },
+      {
+        name: "GitHub",
+        url: "https://github.com/conor-ob",
+      },
+    ],
+  },
+};
---
+++ src/data/sections/experience.ts
@@ -0,0 +1,18 @@
+export const experience = {
+  jobs: [
+    {
+      title: "Senior Software Engineer",
+      company: "Toast",
+      displayDate: "Dec 2018 - Present",
+      logoUrl:
+        "https://cdn.prod.website-files.com/601768b1d530f7448ccc2bfc/601768b1d530f70392cc2c2b_symbol-orange-sq.svg",
+    },
+    {
+      title: "Software Engineer",
+      company: "Murex",
+      displayDate: "Jun 2015 - Nov 2018",
+      logoUrl:
+        "https://cdn.prod.website-files.com/6640cd28f51f13175e577c05/664e00a572c6e1f61064a972_64370701-9cc0-5767-a85b-80cba3c38eee.svg",
+    },
+  ],
+};
