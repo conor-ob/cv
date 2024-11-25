94e4f26630c5e0c118484fc89ff4c0664e570e91
conor-ob <conor.w.ob@gmail.com>
2024-11-25 00:41:11 +0000

    updates

--- src/data/sections/education.ts
+++ src/data/sections/education.ts
@@ -1,13 +1,13 @@
 export const education = {
   degrees: [
     {
-      title: "HDip, Computing - Software Development",
+      title: "HDip, Software Engineering",
       school: "Technological University Dublin",
       displayDate: "2014 - 2015",
       logoUrl: "./tud.png",
     },
     {
-      title: "BEng, Electrical, Electronics and Communications Engineering",
+      title: "BEng, Electronic Engineering",
       school: "University College Dublin",
       displayDate: "2009 - 2013",
       logoUrl: "./ucd.png",
