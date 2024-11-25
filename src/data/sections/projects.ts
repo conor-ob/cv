b4eede4eabd293a31c1c88ca80849741abb91056
conor-ob <conor.w.ob@gmail.com>
2024-11-25 01:23:26 +0000

    add projects

---
+++ src/data/sections/projects.ts
@@ -0,0 +1,28 @@
+export const projects = {
+  apps: [
+    {
+      title: "Putting Pals",
+      type: "React app",
+      logoUrl: "./putting-pals.png",
+      url: "https://puttingpals.conorob.me",
+    },
+    {
+      title: "Club League",
+      type: "React app",
+      logoUrl: "./inter-club-league.png",
+      url: "https://clubleague.conorob.me",
+    },
+    {
+      title: "DubLink",
+      type: "Android app",
+      logoUrl: "./dublink.png",
+      url: "https://github.com/conor-ob/dublink",
+    },
+    {
+      title: "Dublin Bus Pal",
+      type: "Android app",
+      logoUrl: "./dublin-bus-pal.png",
+      url: "https://github.com/conor-ob/dublin-bus-pal",
+    },
+  ],
+};
