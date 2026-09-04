e3c64cd23026a12334fcbd31748c6194f0d7aa90
conor-ob <conor.w.ob@gmail.com>
2026-09-04 16:14:57 +0100

    docker

--- .railway/railway.ts
+++ .railway/railway.ts
@@ -3,6 +3,7 @@ import { defineRailway, github, preserve, project, service } from "railway/iac";
 export default defineRailway(() => {
   const cv = service("cv", {
     source: github("conor-ob/cv", { checkSuites: false }),
+    build: { builder: "DOCKERFILE", dockerfilePath: "Dockerfile" },
     replicas: { "europe-west4-drams3a": 1 },
     deploy: { limitOverride: { containers: { cpu: 1, diskBytes: 100000000000, memoryBytes: 1000000000 } } },
     domains: ["cv.conorob.me"],
